5c843870f6495c68e0a2236ea84e95069847c7e0
chinonso01 <irobindachinonso>
2023-12-05 13:02:04 +0100

    commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
-number = str(random.randint(-10000, 10000))
-lastDigit = int(number[-1:])
+number = random.randint(-10000, 10000)
+lastDigit = int(str(number)[-1:])
 
 if lastDigit > 5:
     result = "and is greater than 5"
@@ -10,4 +10,4 @@ elif lastDigit == 0:
 elif lastDigit < 6 and not 0:
     result = "and is less than 6 and not 0"
     
-print("Last digit of {} is {} {}".format(number, str(lastDigit), result))
+print("Last digit of {} is {} {}".format(number, lastDigit, result))
