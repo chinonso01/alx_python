286beaba700e28707f3101dbe58834a2f4e5e7f7
chinonso01 <irobindachinonso>
2023-12-05 18:02:27 +0100

    commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,11 @@
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
         combination = "{:02}".format(tens_digit * 10 + ones_digit)
-        print(combination, end=", " if ones_digit < 9 else "\n")
+        if ones_digit < 9:
+            print(combination + ", ", end="")
+        else:
+            print(combination)
+
+# To move to the next line after printing all combinations
+print()
+
