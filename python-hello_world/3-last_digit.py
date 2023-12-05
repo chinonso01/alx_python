b55ab318a5767c7c96250fa94fdc16fcbf4ab76b
chinonso01 <irobindachinonso>
2023-12-05 14:34:24 +0100

    commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -5,12 +5,12 @@ lastDigit = str(number)[-1:]
 
 if number < 0:
     lastDigit = str(number)[0] + str(number)[-1]
-    if lastDigit < 6 and not 0:
+    if int(lastDigit) < 6 and not 0:
         result = "and is less than 6 and not 0"
 else:
-    if lastDigit > 5:
+    if int(lastDigit) > 5:
         result = "and is greater than 5"
-    elif lastDigit == 0:
+    elif int(lastDigit) == 0:
         result = "and is 0"
     
 print("Last digit of {} is {} {}".format(number, lastDigit, result))
