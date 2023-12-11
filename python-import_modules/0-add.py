a60f2a21b9d09689638a28fb6022f9bdfb4645ac
chinonso01 <irobindachinonso>
2023-12-11 15:50:41 +0100

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,6 @@
+import add_0
+
 a = 1
 b = 2
 
-add_0 = __import__('add_0')
-result = add_0.add(a, b)
-
-print("{} + {} = {}".format(a, b, result))
+print("{} + {} = {}".format(a, b, add_0.add(a, b)))
