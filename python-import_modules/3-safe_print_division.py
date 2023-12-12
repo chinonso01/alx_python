5780cd3e3e86b7a48187968c2e87400a61fbe18d
chinonso01 <irobindachinonso>
2023-12-12 07:04:08 +0100

    commit

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,15 +2,13 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Division by zero is not allowed.")
         return None
     except Exception as e:
-        print(f"An error occurred: {e}")
         return None
     finally:
-        print("Inside result: {}".format(result))
         return result
     
-a = int(input("Enter a number: "))
-b = int(input("Enter a number: "))
-print("{}".format(safe_print_division(a, b)))
+a = 5
+b = 6
+result = safe_print_division(a, b)
+print("{} / {} = {}".format(a, b, result))
