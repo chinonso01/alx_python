fe11655e013f495149f55b47222aaeb8f0dea9eb
chinonso01 <irobindachinonso>
2023-12-12 05:45:58 +0100

    commit

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,7 +3,7 @@ from sys import argv
 def print_argument(argv):
     num_args = len(argv) - 1
     if  num_args == 0:
-        print("{} arguments".format(num_args))
+        print("{} arguments.".format(num_args))
     elif num_args == 1:
         print("{} argument:".format(num_args))
         print("1: {}".format(argv[1]))
