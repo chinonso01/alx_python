9351d79e71ae923589042a8efe9e7ddf10efd0ef
chinonso01 <irobindachinonso>
2023-12-12 17:46:13 +0100

    commit

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,7 +1,7 @@
 def raise_exception():
-    raise TypeError("This is a custom type exception.")
+    raise TypeError()
 
 try:
     raise_exception()
 except TypeError as e:
-    print(f"Caught an exception: {e}")
+    print(f"Exception raised: {e}")
