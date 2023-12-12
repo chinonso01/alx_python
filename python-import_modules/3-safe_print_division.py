d504b9b697a3607daeeed771e8157930f8c31b1d
chinonso01 <irobindachinonso>
2023-12-12 17:40:05 +0100

    commit

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        result = 0
+        result = None
         return None
     except Exception as e:
         print(f"An error occurred: {e}")
