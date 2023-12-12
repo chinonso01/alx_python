c85647c7323356a7862605e162e1e117eac7b772
chinonso01 <irobindachinonso>
2023-12-12 05:44:55 +0100

    commit

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,16 +1,16 @@
 from sys import argv
 
 def print_argument(argv):
-    num_args = len(argv)
+    num_args = len(argv) - 1
     if  num_args == 0:
         print("{} arguments".format(num_args))
     elif num_args == 1:
-        print("{} argument:\n".format(num_args))
+        print("{} argument:".format(num_args))
         print("1: {}".format(argv[1]))
     else:
-        print("{} arguments:\n".format(num_args))
+        print("{} arguments:".format(num_args))
         i = 1
-        while i < num_args:
+        while i < num_args + 1:
             print("{}: {}".format(i, argv[i]))
             i = i + 1
 
