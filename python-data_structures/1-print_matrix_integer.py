43c2a9d7cb6d8759485ac966b366e90066857c7f
chinonso01 <irobindachinonso>
2023-12-13 11:58:43 +0100

    commit

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
+        for coloum in row:
+            print("{:d}".format(coloum), end=" ")
         print()
