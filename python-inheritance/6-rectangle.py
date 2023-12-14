de8b3531b978b1d037e066ff95e005f35471e0a0
chinonso01 <irobindachinonso>
2023-12-14 20:42:09 +0100

    commit

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Same object module"""
+
+
+class BaseGeometry:
+    """Base class for"""
+    def area(self):
+        """Returns the area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Returns the integer"""
+        if not isinstance(value, int):
+            """Returns"""
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            """Returns"""
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Base class for"""
+    def __init__(self, width, height):
+        """Constructor"""
+        # Validate and set private width and height attributes
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
