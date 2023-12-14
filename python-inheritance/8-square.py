c93489234ce23a3903d102bcbfcd032cfb960815
chinonso01 <irobindachinonso>
2023-12-14 20:56:12 +0100

    commit

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -9,12 +9,12 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Returns the integer"""
+        """"Returns the integer"""
         if not isinstance(value, int):
             """Returns"""
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            """Returns"""
+            """Returns the"""
             raise ValueError(f"{name} must be greater than 0")
 
 
@@ -22,24 +22,27 @@ class Rectangle(BaseGeometry):
     """Base class for"""
     def __init__(self, width, height):
         """Constructor"""
-        # Validate and set private width and height attributes
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
+        """Returns"""
         return self.__width * self.__height
 
     def __str__(self):
+        """Returns"""
         return f"[Rectangle] {self.__width}/{self.__height}"
 
 
 class Square(Rectangle):
+    """Base class for square rectangles"""
     def __init__(self, size):
-        # Validate and set private size attribute
+        """Constructor"""
         self.integer_validator("size", size)
         super().__init__(size, size)
 
     def __str__(self):
-        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
+        """Returns a string representation of"""
+        return f"[Square] {self.__width}/{self.__height}"
