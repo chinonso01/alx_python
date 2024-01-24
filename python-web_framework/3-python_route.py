38b12e73db21976912ebd341514afc68045879da
chinonso01 <irobindachinonso>
2024-01-24 18:25:59 +0100

    commit

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+"""
+    Basic flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C " + text.replace('_', ' ')
+
+@app.route('/python/<text>')
+def python(text='cool'):
+    return "Python " + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
