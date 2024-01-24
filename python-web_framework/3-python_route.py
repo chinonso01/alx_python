4747664075da3d9654956bb6be67cd2f58a7f5d6
chinonso01 <irobindachinonso>
2024-01-24 18:37:36 +0100

    commit

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -19,7 +19,8 @@ def hbnb():
 def c(text):
     return "C " + text.replace('_', ' ')
 
-@app.route('/python/<text>', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python(text):
     return "Python " + text.replace('_', ' ')
 
