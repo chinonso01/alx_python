fb1ef3081f8613a8ba5d70f6f5fc5212f729159d
chinonso01 <irobindachinonso>
2024-01-25 05:23:29 +0100

    commit

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -29,7 +29,8 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', n=n)
+    number = f'Number: {n}'
+    return render_template('5-number.html', number=number)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
