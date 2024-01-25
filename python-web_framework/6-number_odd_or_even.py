0ba1126ac9fedc74531ba0752635207cbb38e87c
chinonso01 <irobindachinonso>
2024-01-25 05:49:36 +0100

    commit

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -34,11 +34,9 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     if n % 2 == 0:
-        odd_or_even = f'Number: {n} is even' 
-        return 'even' 
+        odd_or_even = f'Number: {n} is even'  
     elif n % 2 != 0:
         odd_or_even = f'Number: {n} is odd'
-        return 'odd'
     else:
         return 404
     return render_template('6-number_odd_or_even.html', odd_or_even=odd_or_even)
