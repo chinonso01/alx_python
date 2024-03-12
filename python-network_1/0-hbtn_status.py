fb15c8f73a4b48054d22daf87424784af78dd2ea
chinonso01 <irobindachinonso>
2024-03-12 10:50:38 +0100

    commit

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,6 @@
+import requests
+
+response = requests.get('https://alu-intranet.hbtn.io/status')
+print('Body response:')
+print('\t- type:', type(response.text))
+print('\t- content:', response.text)
