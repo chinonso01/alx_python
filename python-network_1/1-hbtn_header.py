1b96b0f28aedfd637412e8b3b2e6490fd9def22a
chinonso01 <irobindachinonso>
2024-03-12 12:42:36 +0100

    commit

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 script_name.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    if 'X-Request-Id' in response.headers:
+        request_id = response.headers['X-Request-Id']
+        print(request_id)
+    else:
+        print("X-Request-Id not found in the response headers")
+
+if __name__ == "__main__":
+    main()
