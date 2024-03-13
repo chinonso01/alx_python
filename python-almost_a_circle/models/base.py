7f7d6e880433393f638d5f8b4b750d6d14494d74
chinonso01 <irobindachinonso>
2024-03-13 01:43:56 +0100

    commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,16 @@
 # models/base.py
+"""Defines the Base class."""
 
 class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Initialize a Base instance.
+
+        Args:
+            id (int, optional): The identifier for the instance. Defaults to None.
+        """
         if id is not None:
             self.id = id
         else:
