49c23c674f1d1a85ccc14babc72c3f3c391a6543
chinonso01 <irobindachinonso>
2024-03-13 19:11:46 +0100

    commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,7 @@ class Rectangle(Base):
         """Print the rectangle using '#' characters."""
         for _ in range(self.height):
             print('#' * self.width)
+
+    def __str__(self):
+        """Return a string representation of the rectangle."""
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
