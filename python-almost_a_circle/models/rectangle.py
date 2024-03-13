c7bdbffa3fe9e3ac7b8e87675c571a8636a735a1
chinonso01 <irobindachinonso>
2024-03-13 19:14:01 +0100

    commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -82,7 +82,9 @@ class Rectangle(Base):
 
     def display(self):
         """Print the rectangle using '#' characters."""
+        print("\n" * self.y, end="")
         for _ in range(self.height):
+            print(" " * self.x, end="")
             print('#' * self.width)
 
     def __str__(self):
