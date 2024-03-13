f5e61412fd86d84bc5a0d7dd7977938d10f94509
chinonso01 <irobindachinonso>
2024-03-13 19:24:31 +0100

    commit

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -19,6 +19,17 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return a string representation of the square."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
