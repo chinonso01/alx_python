a9dddedff58b52ad0c48383eb5628c5894afc344
chinonso01 <irobindachinonso>
2024-03-13 19:09:28 +0100

    commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,8 @@ class Rectangle(Base):
     def area(self):
         """Calculate the area of the rectangle."""
         return self.width * self.height
+
+    def display(self):
+        """Print the rectangle using '#' characters."""
+        for _ in range(self.height):
+            print('#' * self.width)
